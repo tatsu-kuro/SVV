1fe621a59b7a50cd4e62ad937b05d27d3dd8392d
Tatsuaki Kuroda <cocoa@kuroda33.com>
2019-07-03 15:56:40 +0900

    Initial Commit

---
+++ SVV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SVV
+//
+//  Created by kuroda tatsuaki on 2019/07/03.
+//  Copyright © 2019 tatsuaki.kuroda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
